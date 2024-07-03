ddaf0fa9f17c43679852f2899f67aa465702d067
suwumonster <20212121013@m.scnu.edu.cn>
2024-07-03 12:01:06 +0800

    LCR180

---
+++ LC/LCR180v1.cpp
@@ -0,0 +1,35 @@
+bool comp(const vector<int> a, const vector<int> b)
+{
+    return a[0] < b[0];
+}
+class Solution {
+public:
+
+    vector<vector<int>> fileCombination(int target) {
+        vector<vector<int>> ret;
+        size_t sum = 0;
+        for (size_t i = 2; i < target; i++)//i 文件数量 即 累加个数
+        {
+            sum = 0;
+            for (size_t j = 1; sum < target; j++)//j 从 j 开始往后累加
+            {
+                sum = 0;
+                for (size_t k = 0; k < i; k++)
+                {
+                    sum += j + k;
+                }
+                if (sum == target)
+                {
+                    vector<int> tmp;
+                    for (size_t k = 0; k < i; k++)
+                    {
+                        tmp.push_back(j + k);
+                    }
+                    ret.push_back(tmp);
+                }
+            }
+        }
+        sort(ret.begin(), ret.end(), comp);
+        return ret;
+    }
+};
