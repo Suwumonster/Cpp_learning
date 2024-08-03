08e3ab2a9c314097e018432a4541632cb3586fbf
suwumonster <20212121013@m.scnu.edu.cn>
2024-08-03 09:46:55 +0800

    fixed Erase, add InsertR, FindR, copy constructor, operator=...

--- 模拟实现BST/BST.h
+++ 模拟实现BST/BST.h
@@ -17,11 +17,27 @@ namespace my_stl
 	{
 		typedef BSTreeNode<K> Node;
 	public:
+		BST() = default;
+		BST(const BST<K>& t)
+		{
+			_root = _Copy(t._root);
+		}
+		~BST()
+		{
+			_destory(_root);
+		}
+		BST<K>& operator=(BST<K> t)
+		{
+			std::swap(_root, t._root);
+			return *this;
+		}
+		//常规循环版本 插入删除查找 实际开发中推荐此版本
 		bool Insert(const K& key)
 		{
 			if (_root == nullptr)
 			{
 				_root = new Node(key);
+				return true;
 			}
 			Node* cur = _root;
 			Node* parent = nullptr;
@@ -52,21 +68,7 @@ namespace my_stl
 			}
 			return true;
 		}
-		void InOrder()
-		{
-			_InOrder(_root);
-			cout << endl;
-		}
-		void _InOrder(Node* root)
-		{
-			if (root == nullptr)
-			{
-				return;
-			}
-			_InOrder(root->_left);
-			cout << root->_key << " ";
-			_InOrder(root->_right);
-		}
+		
 		Node* Find(const K& key)
 		{
 			Node* cur = _root;
@@ -91,7 +93,7 @@ namespace my_stl
 		bool Erase(const K& key)
 		{
 			Node* DelNode = _root;
-			Node* parent = _root;
+			Node* parent = nullptr;
 			while (DelNode)
 			{
 				if (DelNode->_key < key)
@@ -106,31 +108,13 @@ namespace my_stl
 				}
 				else
 				{
-					//delNode 左右子树为空
-					if (DelNode->_left == nullptr && DelNode->_right == nullptr)
+					//delNode 左右子树为空 or delNode 左子树为空 右子树不为空
+					//两种情况可以统一处理 直接删除法
+					if (DelNode->_left == nullptr)
 					{
-						if (parent != DelNode)//删除结点不是最后一个结点
+						if (parent != nullptr)//删除结点不是根结点
 						{
-							if (parent->_left && parent->_left->_key == DelNode->_key)
-							{
-								parent->_left = nullptr;
-							}
-							else
-							{
-								parent->_right = nullptr;
-							}
-						}
-						else
-						{
-							_root = nullptr;
-						}
-						delete DelNode;
-					}
-					else if (DelNode->_left == nullptr)//delNode 左子树为空 右子树不为空
-					{
-						if (parent != DelNode)//删除结点时 n > 2
-						{
-							if (parent->_left && parent->_left->_key == DelNode->_key)
+							if (parent->_left == DelNode)
 							{
 								parent->_left = DelNode->_right;
 							}
@@ -139,17 +123,17 @@ namespace my_stl
 								parent->_right = DelNode->_right;
 							}
 						}
-						else//删除结点时 n <= 2
+						else//删除结点是根节点
 						{
 							_root = _root->_right;
 						}
 						delete DelNode;
 					}
-					else if (DelNode->_right == nullptr)//delNode 右子树为空 左子树不为空
+					else if (DelNode->_right == nullptr)//delNode 右子树为空 左子树不为空 直接删除法
 					{
-						if (parent != DelNode)//删除结点时 n > 2
+						if (parent != nullptr)//删除结点不是根结点
 						{
-							if (parent->_left && parent->_left->_key == DelNode->_key)
+							if (parent->_left == DelNode)
 							{
 								parent->_left = DelNode->_left;
 							}
@@ -158,16 +142,17 @@ namespace my_stl
 								parent->_right = DelNode->_left;
 							}
 						}
-						else//删除结点时 n <= 2
+						else//删除结点是根结点
 						{
 							_root = _root->_left;
 						}
 						delete DelNode;
 					}
-					else//delNode 左右子树都不为空
+					else//delNode 左右子树都不为空 间接删除法 通过找到 maxLeft 或者 minRight 
+						//与删除节点进行KEY替换 删除节点 删除
 					{
 						//左子树最大结点 or 右子树最小结点 可以替代 delnode
-						//默认 是 右子树最小结点
+						//默认 是 右子树最小结点 即 右子树的最左结点
 						Node* minRight = DelNode->_right;
 						Node* PminRight = DelNode;
 						while (minRight->_left)
@@ -177,33 +162,17 @@ namespace my_stl
 						}
 
 						DelNode->_key = minRight->_key;
-						//删除minright，但是minright有可能有右子树
-
-						//处理minright的右子树
-						if (minRight->_right)
+						
+						//minright肯定没有左节点；
+						//右节点可能有，可能没有。
+						//处理minright的右节点
+						if (PminRight->_left == minRight)
 						{
-							Node* minRight_right = minRight->_right;
-							if (PminRight->_left && PminRight->_left->_key == minRight->_key)
-							{
-								PminRight->_left = minRight_right;
-							}
-							else
-							{
-								PminRight->_right = minRight_right;
-							}
-							
+							PminRight->_left = minRight->_right;
 						}
 						else
 						{
-							//将minRight置空
-							if (PminRight->_left && PminRight->_left->_key == minRight->_key)
-							{
-								PminRight->_left = nullptr;
-							}
-							else
-							{
-								PminRight->_right = nullptr;
-							}
+							PminRight->_right = minRight->_right;
 						}
 
 						//删除minright结点
@@ -214,14 +183,144 @@ namespace my_stl
 			}
 			return false;
 		}
-		
-		~BST()
+		//递归版本 插入删除查找
+
+		bool InsertR(const K& key)
 		{
-			while (_root)
+			return _InsertR(_root, key);
+		}
+
+		Node* FindR(const K& key)
+		{
+			return _FindR(_root, key);
+		}
+
+		bool EarseR(const K& key)
+		{
+			return _EarseR(_root, key);
+		}
+		void InOrder()
+		{
+			_InOrder(_root);
+			cout << endl;
+		}
+
+	protected:
+		
+		Node* _Copy(Node* root)//拷贝构造用
+		{
+			if (root == nullptr)
 			{
-				Erase(_root->_key);
+				return nullptr;
+			}
+			Node* NewRoot = new Node(root->_key);
+			NewRoot->_left = _Copy(root->_left);
+			NewRoot->_right = _Copy(root->_right);
+			return NewRoot;
+		}
+		void _destory(Node*& root)//析构函数用
+		{
+			if (root == nullptr)
+			{
+				return;
+			}
+			_destory(root->_left);
+			_destory(root->_right);
+			delete root;
+			root = nullptr;
+		}
+		void _InOrder(Node* root)//中序遍历用
+		{
+			if (root == nullptr)
+			{
+				return;
+			}
+			_InOrder(root->_left);
+			cout << root->_key << " ";
+			_InOrder(root->_right);
+		}
+
+		bool _InsertR(Node*& root, const K& key)
+		{
+			if (root == nullptr)
+			{
+				root = new Node(key);
+				return true;
+			}
+			if (root->_key > key)
+			{
+				return _InsertR(root->_left, key);
+			}
+			else if (root->_key < key)
+			{
+				return _InsertR(root->_right, key);
+			}
+			else
+			{
+				return false;
 			}
 		}
+		Node* _FindR(Node* root, const K& key)
+		{
+			if (root == nullptr)
+			{
+				return nullptr;
+			}
+			if (root->_key > key)
+			{
+				return _FindR(root->_left, key);
+			}
+			else if (root->_key < key)
+			{
+				return _FindR(root->_right, key);
+			}
+			else
+			{
+				return root;
+			}
+		}
+		bool _EarseR(Node*& root, const K& key)
+		{
+			if (root == nullptr)
+			{
+				return false;
+			}
+			if (root->_key > key)
+			{
+				return _EarseR(root->_left, key);
+			}
+			else if (root->_key < key)
+			{
+				return _EarseR(root->_right, key);
+			}
+			else
+			{
+				Node* del = root;
+				if (del->_left == nullptr)
+				{
+					root = del->_right;
+				}
+				else if (del->_right == nullptr)
+				{
+					root = del->_left;
+				}
+				else
+				{
+					Node* maxLeft = del->_left;
+					while (maxLeft->_right)
+					{
+						maxLeft = maxLeft->_right;
+					}
+
+					root->_key = maxLeft->_key;
+
+					return _EarseR(root->_left, root->_key);
+				}
+				delete del;
+				return true;
+			}
+		}
+
 	private:
 		Node* _root = nullptr;
 	};
--- 模拟实现BST/BSTtest.cpp
+++ 模拟实现BST/BSTtest.cpp
@@ -15,10 +15,11 @@ namespace my_stl
 		t.Insert(8);
 		t.InOrder();
 
-		t._InOrder(t.Find(3));
-		t._InOrder(t.Find(12));
+		//t.Find(3)->_key = 4;
+		t.InOrder();
+
 	}
-	void test2()//Erase
+	void test2()//Erase 析构函数 test
 	{
 		BST<int> t;
 		int a[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
@@ -28,7 +29,80 @@ namespace my_stl
 		}
 		t.InOrder();
 
-		t.~BST();
+		/*t.~BST();*/
+
+		t.Erase(4);
+		t.InOrder();
+
+		t.Erase(6);
+		t.InOrder();
+
+		t.Erase(14);
+		t.InOrder();
+
+		t.Erase(3);
+		t.InOrder();
+
+		t.Erase(8);
+		t.InOrder();
+
+		t.Erase(1);
+		t.InOrder();
+
+		t.Erase(10);
+		t.InOrder();
+
+		t.Erase(13);
+		t.InOrder();
+
+		t.Erase(7);
+		t.InOrder();
+	}
+
+
+	void test3()//test 拷贝构造 赋值重载
+	{
+		BST<int> t1;
+		int a[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
+		for (int i = 0; i < sizeof(a) / sizeof(int); i++)
+		{
+			t1.Insert(a[i]);
+		}
+
+		t1.InOrder();
+		cout << endl;
+
+		BST<int> t2(t1);
+
+		t1.Insert(12);
+		t1.InOrder();
+		t2.InOrder();
+		cout << endl;
+
+		BST<int> t3;
+		t3 = t1;
+		t1.Erase(14);
+		t1.InOrder();
+		t2.InOrder();
+		t3.InOrder();
+		cout << endl;
+	}
+
+	void test4()//测试递归版本的增删查
+	{
+		BST<int> t;
+		int a[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
+		for (int i = 0; i < sizeof(a) / sizeof(int); i++)
+		{
+			t.InsertR(a[i]);
+		}
+		t.InsertR(8);
+		t.InOrder();
+
+		/*t.FindR(3)->_key = 4;
+		t.InOrder();*/
+
+		/*t.~BST();*/
 
 		t.Erase(4);
 		t.InOrder();
@@ -61,6 +135,6 @@ namespace my_stl
 
 int main()
 {
-	my_stl::test2();
+	my_stl::test4();
 	return 0;
 }
